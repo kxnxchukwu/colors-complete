23c75ad7e1b1d29182ffa2846f4947957fb17268
Kenechukwu <kenechukwu.ajufo@gmail.com>
2023-02-21 15:35:42 +0000

    Updated Application Component to use Color Reducer

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -1,21 +1,29 @@
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
 import SavedColors from './saved-colors';
 import RelatedColors from './related-colors';
 import AdjustColors from './adjust-colors';
 import ColorPicker from './color-picker';
+import { colorReducer, initialState } from '../color-reducer';
 
 const Application = () => {
-  const [hexColor, setHexColor] = useState('#e56e24');
+  const [{ hexColor }, dispatch] = useReducer(colorReducer, initialState);
 
   return (
-    <div className="grid max-w-3xl grid-cols-1 gap-8 p-8 pb-40 mx-auto dark:bg-slate-900 dark:text-white sm:grid-cols-2">
+    <div className="mx-auto grid max-w-3xl grid-cols-1 gap-8 p-8 pb-40 dark:bg-slate-900 dark:text-white sm:grid-cols-2">
       <ColorPicker
         hexColor={hexColor}
-        onChange={(e) => setHexColor(e.target.value)}
+        onChange={(e) =>
+          dispatch({
+            type: 'update-hex-color',
+            payload: {
+              hexColor: e.target.value,
+            },
+          })
+        }
       />
-      <AdjustColors hexColor={hexColor} />
-      <RelatedColors hexColor={hexColor} />
-      <SavedColors hexColor={hexColor} />
+      <AdjustColors dispatch={dispatch} hexColor={hexColor} />
+      <RelatedColors dispatch={dispatch} hexColor={hexColor} />
+      <SavedColors dispatch={dispatch} hexColor={hexColor} />
     </div>
   );
 };
