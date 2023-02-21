a444da11565c094775e6498c6acb1f5210e72dde
Kenechukwu <kenechukwu.ajufo@gmail.com>
2023-02-21 15:37:14 +0000

    Add functionality to HexToRGB/HexToHSL components

--- src/components/adjust-colors/index.tsx
+++ src/components/adjust-colors/index.tsx
@@ -1,19 +1,22 @@
+import { Dispatch } from 'react';
 import ColorName from './color-name';
 import HexToCMYK from './to-cmyk';
 import HexToHSL from './to-hsl';
 import HexToHSV from './to-hsv';
 import HexToRGB from './to-rgb';
+import { AdjustColorActions } from '../../color-reducer';
 
 type AdjustColorsProps = {
   hexColor: string;
+  dispatch: Dispatch<AdjustColorActions>;
 };
 
-const AdjustColors = ({ hexColor }: AdjustColorsProps) => {
+const AdjustColors = ({ hexColor, dispatch }: AdjustColorsProps) => {
   return (
-    <div className="flex flex-col w-full gap-4">
+    <div className="flex w-full flex-col gap-4">
       <h3>Adjust Colors</h3>
-      <HexToRGB hexColor={hexColor} />
-      <HexToHSL hexColor={hexColor} />
+      <HexToRGB dispatch={dispatch} hexColor={hexColor} />
+      <HexToHSL dispatch={dispatch} hexColor={hexColor} />
       <HexToHSV hexColor={hexColor} />
       <HexToCMYK hexColor={hexColor} />
       <ColorName hexColor={hexColor} />
